5494e55e8ec9d3330102b9ce32ba5c0cc650e093
Abhishek <abhishekdtu6@gmail.com>
2018-01-25 17:22:55 +0530

    First Commit

---
+++ fsm_1.scala
@@ -0,0 +1,19 @@
+// Design of fsm_1: Deals with the transfer of data between DSM and vec register file
+
+package fsm_1
+
+import Chisel._
+
+object fsmDefines {
+
+}
+
+
+class fsmData extends Bundle {
+	val fsmReady = Bool(false)
+	val fsmSync  = Bool(false)
+}
+
+class fsm_1 extends Module{
+
+}
